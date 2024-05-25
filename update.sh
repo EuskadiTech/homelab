a927838bbdd999c35c0cf5b20c0b9940d957044e
Nulldev <109038805+naielv@users.noreply.github.com>
2024-05-25 13:11:29 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,5 @@
 git pull
-COMPOSE_FILE=docker/nginx-proxy-manager.yml docker compose up
-COMPOSE_FILE=docker/homer.yml docker compose up
-COMPOSE_FILE=docker/portainer.yml docker compose up
-COMPOSE_FILE=docker/uptime-kuma.yml docker compose up
-
+COMPOSE_FILE=docker/nginx-proxy-manager.yml docker compose up -d
+COMPOSE_FILE=docker/homer.yml docker compose up -d
+COMPOSE_FILE=docker/portainer.yml docker compose up -d
+COMPOSE_FILE=docker/uptime-kuma.yml docker compose up -d
